181de3275d42ba942b57232cd8eeb5143898c8ce
Dynamo Stack <9chakri+ds@gmail.com>
2016-11-24 01:22:46 -0800

    update

---
+++ client.py
@@ -0,0 +1,72 @@
+from socketIO_client import SocketIO, LoggingNamespace
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from textblob import TextBlob
+import os
+import json
+
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey=os.environ.get('ckey')
+csecret=os.environ.get('csecret')
+atoken=os.environ.get('atoken')
+asecret=os.environ.get('asecret')
+score=0
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+
+
+class listener(StreamListener):
+
+	def __init__(self, cb):
+		self.cb=cb
+
+	def on_error(self, status):
+		print status
+
+	def on_data(self,data):
+		all_data = json.loads(data)
+		tweet = all_data["text"]
+		username = all_data['user']['screen_name']
+		analysis = TextBlob(tweet)
+		#print(analysis.sentiment.polarity)
+		return self.cb(analysis,tweet)
+
+
+class Namespace(LoggingNamespace):
+    _connected = True
+
+    def on_connect(self):
+        print('connected')
+
+    def on_error(self, data):
+        print('error')
+
+
+
+socketIO = SocketIO('localhost', 8000, Namespace)
+nsp = socketIO.define(Namespace, '/chat')
+    #socketIO.wait(seconds=1)
+    #socketIO.wait(seconds=1)
+print("invoking ..1")
+
+def cb2(alysys,tweet):
+    global score
+    #print(float(alysys.sentiment.polarity))
+    score+= float(alysys.sentiment.polarity)
+    nsp.emit('chat',{'score':score,'tweet':tweet},namespace='/chat')
+
+twitterStream = Stream(auth, listener(cb2))
+twitterStream.filter(track=["noteban","demonetisation","bharatbandh"],async=True)
+
+print("invoking ..2")
+
+    
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,44 @@
+import socketio
+import eventlet
+import eventlet.wsgi
+from flask import Flask, render_template
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+
+
+#API
+@app.route('/')
+def index():
+    """Serve the client-side application."""
+    return render_template('index.html')
+
+@sio.on('connect', namespace='/chat')
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.on('chat', namespace='/chat')
+def message(sid, data):
+    sio.emit('reply', data,namespace='/chat')
+
+@sio.on('disconnect', namespace='/chat')
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+
+
+
+
+  
+
+
+
+
+
